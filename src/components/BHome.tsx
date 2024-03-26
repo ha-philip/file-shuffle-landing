d79109a3d88ce1bbd278d1a464ebd0f4e14a429f
haphilip <surroundai@gmail.com>
2024-03-26 15:23:43 +0900

    변경

--- src/components/BHome.tsx
+++ src/components/BHome.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function BHome() {
   return (
     <div className="섹터B">
-      <h1>IObit Uninstaller 13 Free</h1>
+      <h1>fffftaller 13 Free</h1>
       <h2>
         A light and free solution to scan, manage and delete software in bulk.<br/>
         Completely uninstall all unwanted programs, stubborn programs, and
